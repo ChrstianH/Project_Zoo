407dbb05954e472ad027ed3e657a87bd974c6a2c
AliceReturns <Shanisaglass@gmail.com>
2024-08-21 10:09:57 +0200

    ID Änderung

--- src/classes/Enclosure.ts
+++ src/classes/Enclosure.ts
@@ -1,11 +1,11 @@
 import EnclosureId from "./IDs";
 
 class Enclosure {
-  id: number;
+  id: EnclosureId;
   name: string;
   builtInYear: number;
 
-  constructor(id: number, name: string, builtInYear: number) {
+  constructor(id: EnclosureId, name: string, builtInYear: number) {
     this.id = id;
     this.name = name;
     this.builtInYear = builtInYear;
